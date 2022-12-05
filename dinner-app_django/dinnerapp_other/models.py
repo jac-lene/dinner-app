9336729620249b24a8080e8378f8c97236d5c4b7
jac-lene <jaclenesini@gmail.com>
2022-12-05 15:44:41 -0500

    profile schema

--- dinner-app_django/dinnerapp_other/models.py
+++ dinner-app_django/dinnerapp_other/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    profileImage = models.TextField()
+    bannerImage = models.TextField()
+    pronouns = models.CharField(max_length=200)
+    orientation = models.CharField(max_length=200)
+    gender = models.CharField(max_length=200)
+    age = models.IntegerField(max_length=3)
+    profession = models.CharField(max_length=200)
+    about = models.TextField()
+    city = models.CharField(max_length=200)
+    isHost = models.BooleanField()
+    dinners = models.ManyToOneRel()
+    user = models.OneToOneField()
+
+    def __str__(self):
+        return self.name

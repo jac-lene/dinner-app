dc9459d8a1eee29e6420a44838b0e031c9ca6325
jac-lene <jaclenesini@gmail.com>
2022-12-05 15:51:50 -0500

    models made

--- dinner-app_django/dinnerapp_other/models.py
+++ dinner-app_django/dinnerapp_other/models.py
@@ -13,7 +13,38 @@ class Profile(models.Model):
     city = models.CharField(max_length=200)
     isHost = models.BooleanField()
     dinners = models.ManyToOneRel()
-    user = models.OneToOneField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Chat(models.Model):
+    subject = models.CharField(max_length=200)
+    dinner = models.ManyToOneRel()
+    userName = models.OneToOneField()
+    hostName = models.OneToOneField()
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    text = models.TextField()
+    time = models.DateTimeField()
+    userName = models.OneToOneField()
+    hostName = models.OneToOneField()
+
+    def __str__(self):
+        return self.name
+
+class Dinner(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    time = models.DateTimeField()
+    attendees = models.ManyToOneRel()
+    location = models.TextField()
+    capacity = models.IntegerField(max_length=2)
+    isPublic = models.BooleanField()
 
     def __str__(self):
         return self.name

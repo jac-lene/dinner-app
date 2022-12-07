8b413da2ee9cee44bd2b1a66b681cfd61e631226
aparks4 <arynmparks@gmail.com>
2022-12-07 15:50:22 -0800

    added token routes

--- dinner-app_django/dinnerapp_other/urls.py
+++ dinner-app_django/dinnerapp_other/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
 urlpatterns = [
     path('users/', views.Users.as_view(), name="users"),
     path('users/<int:pk>', views.UserDetail.as_view(), name="user_detail"),
     path('register/', views.RegisterView.as_view(), name="auth_register"),
+    path('token/', views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('token/refresh/', views.TokenRefreshView.as_view(), name="token_refresh"),
     path('profiles/', views.ProfileList.as_view(), name="profile_list"),
     path('profiles/<int:pk>/', views.ProfileDetail.as_view(), name="profile_detail"),
     path('dinners/', views.DinnerList.as_view(), name="dinner_list"),

bfbbe0e0c5ef0f39b6adc941d4c4c8d07d8d3700
Jordan-Burgess <alfredjordan93@gmail.com>
2022-12-07 14:12:43 -0500

    Make email and username unique

--- dinner-app_django/dinnerapp_other/models.py
+++ dinner-app_django/dinnerapp_other/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
 
-
+User._meta.get_field('email')._unique = True
+User._meta.get_field('username')._unique = True
 
 class Profile(models.Model):
     pronouns = models.CharField(max_length=200)
